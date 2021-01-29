588faf8764946a431b8dceb861322d3857b6f2b7
IanMarcony <imarconyls@gmail.com>
2021-01-29 02:23:45 -0400

    Added initial backend

---
+++ src/main/java/com/marcony/osmarcony/OsmarconyApiApplication.java
@@ -0,0 +1,14 @@
+package com.marcony.osmarcony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsmarconyApiApplication {
+
+	public static void main(String[] args) {		
+		SpringApplication.run(OsmarconyApiApplication.class, args);
+		
+	}
+
+}
